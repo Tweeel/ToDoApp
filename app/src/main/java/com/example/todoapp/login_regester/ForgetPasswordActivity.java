25f2aefffd14323c20eea7b779aaefb820b0d1c8
Baba Bendermel Houssem Eddine <75279465+Tweeel@users.noreply.github.com>
2021-12-05 19:26:53 +0100

    organization

--- app/src/main/java/com/example/todoapp/login_regester/ForgetPasswordActivity.java
+++ app/src/main/java/com/example/todoapp/login_regester/ForgetPasswordActivity.java
@@ -1,4 +1,4 @@
-package com.example.todoapp;
+package com.example.todoapp.login_regester;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
@@ -7,26 +7,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
 
-public class ResetPasswordActivity extends AppCompatActivity {
+import com.example.todoapp.R;
+
+public class ForgetPasswordActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reset_password);
+        setContentView(R.layout.activity_forget_password);
 
         //setup rollback button
         ImageView rollaback = findViewById(R.id.rollback);
         rollaback.setOnClickListener(v -> {
-            Intent intentRollBack = new Intent(this, ForgetPasswordActivity.class);
+            Intent intentRollBack = new Intent(this, LoginActivity.class);
             startActivity(intentRollBack);
         });
 
-        //setup send change password button
-        AppCompatButton change_btn = (AppCompatButton) findViewById(R.id.change_btn);
-        change_btn.setOnClickListener(v -> {
-            Intent intentRequest = new Intent(this, SuccesfulActivity.class);
+        //setup send request code button
+        AppCompatButton sendREQUEST = (AppCompatButton) findViewById(R.id.request_btn);
+        sendREQUEST.setOnClickListener(v -> {
+            Intent intentRequest = new Intent(this, ResetPasswordActivity.class);
             startActivity(intentRequest);
-            finish();
         });
     }
 }
--- app/src/main/java/com/example/todoapp/login_regester/RegistrationActivity.java
+++ app/src/main/java/com/example/todoapp/login_regester/RegistrationActivity.java
@@ -1,12 +1,13 @@
-package com.example.todoapp;
+package com.example.todoapp.login_regester;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.TextView;
+
+import com.example.todoapp.R;
 
 public class RegistrationActivity extends AppCompatActivity {
 
